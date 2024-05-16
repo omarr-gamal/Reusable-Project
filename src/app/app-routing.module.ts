c51f4cbb09800df2712ae59ea985f695405e503a
Omar Emad <2001omaremad@gmail.com>
2024-05-16 04:57:01 +0300

    interval to run get weather

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [authGuard] },
+  // { path: '', component: HomeComponent, canActivate: [authGuard] },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
   { path: '**', redirectTo: '' },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { AuthService } from '../../services/auth.service';
 import { CurrentWeatherData } from '../../models/weather-data.model';
 import { WeatherApiService } from '../../services/weather-api.service';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   location: string = 'Cairo'; // Default location
   weatherData: CurrentWeatherData | null = null;
+  private weatherSubscription: Subscription | null = null;
+  private intervalId: any;
 
-  constructor(private weatherApiService: WeatherApiService) {
-    this.weatherApiService.getWeather(this.location).subscribe(data => {
-      this.weatherData = data;
-    });
+  constructor(private weatherApiService: WeatherApiService) { }
+
+  ngOnInit(): void {
+    this.getWeather(this.location);
+
+    // Set up an interval to fetch weather data every 10 minutes
+    this.intervalId = setInterval(() => {
+      this.getWeather(this.location);
+    }, 60000 * 10); // 60000 milliseconds = 1 minute
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    // Unsubscribe from the weather API if there's an active subscription
+    if (this.weatherSubscription) {
+      this.weatherSubscription.unsubscribe();
+    }
   }
 
   getWeather(location: string): void {
-    this.weatherApiService.getWeather(location).subscribe(data => {
+    console.log('hehe');
+    this.weatherSubscription = this.weatherApiService.getWeather(location).subscribe(data => {
       this.weatherData = data;
     });
   }
